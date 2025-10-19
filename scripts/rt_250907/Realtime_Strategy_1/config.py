bde85e2a3c12891fff1e414c38b5215d01fc2009
IgnazHuber <43025615+IgnazHuber@users.noreply.github.com>
2025-10-19 15:48:58 +0200

    Delete scripts/rt_250907/Realtime_Strategy_1/config.py

--- scripts/rt_250907/Realtime_Strategy_1/config.py
+++
@@ -1,3 +0,0 @@
-#--------------------DEMO Account--------------------------------------------
-BYBIT_DEMO_API_KEY = "Baw9t9dT14EEeTVWug"
-BYBIT_DEMO_API_SECRET = "7e3cSlHGDKyAbYy2HNsCsYi1pgb2sVEZdkHJ"
